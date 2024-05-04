658e5954e8ebe6e55f62d6a7688724a2dfb97b97
Brittany Tinsley <bktinsley90@gmail.com>
2024-05-04 09:07:09 -0400

    completed modifyProductForm, userinterface complete

---
+++ Brittany_wguC968/modifyProductForm.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Brittany_wguC968
+{
+    partial class modifyProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label8 = new Label();
+            cancelProductBtn = new Button();
+            saveProductBtn = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            textBox7 = new TextBox();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            label9 = new Label();
+            addProductBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(21, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Modify Product";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(37, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(18, 15);
+            label2.TabIndex = 1;
+            label2.Text = "ID";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(37, 128);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(37, 160);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Inventory";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(37, 209);
+            label5.Name = "label5";
+            label5.Size = new Size(33, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Price";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(37, 252);
+            label6.Name = "label6";
+            label6.Size = new Size(30, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Max";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(171, 252);
+            label7.Name = "label7";
+            label7.Size = new Size(28, 15);
+            label7.TabIndex = 6;
+            label7.Text = "Min";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(99, 82);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(99, 120);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(99, 152);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(99, 201);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(99, 244);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(46, 23);
+            textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(205, 244);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(54, 23);
+            textBox6.TabIndex = 12;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(377, 23);
+            label8.Name = "label8";
+            label8.Size = new Size(107, 15);
+            label8.TabIndex = 13;
+            label8.Text = "All Candidate Parts";
+            // 
+            // cancelProductBtn
+            // 
+            cancelProductBtn.Location = new Point(713, 453);
+            cancelProductBtn.Name = "cancelProductBtn";
+            cancelProductBtn.Size = new Size(75, 23);
+            cancelProductBtn.TabIndex = 14;
+            cancelProductBtn.Text = "Cancel";
+            cancelProductBtn.UseVisualStyleBackColor = true;
+            // 
+            // saveProductBtn
+            // 
+            saveProductBtn.Location = new Point(607, 453);
+            saveProductBtn.Name = "saveProductBtn";
+            saveProductBtn.Size = new Size(75, 23);
+            saveProductBtn.TabIndex = 15;
+            saveProductBtn.Text = "Save";
+            saveProductBtn.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(713, 424);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 16;
+            button3.Text = "Delete";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(607, 15);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 17;
+            button4.Text = "Search";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(688, 15);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 18;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(377, 44);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(411, 150);
+            dataGridView1.TabIndex = 19;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(377, 266);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(411, 150);
+            dataGridView2.TabIndex = 20;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(377, 244);
+            label9.Name = "label9";
+            label9.Size = new Size(190, 15);
+            label9.TabIndex = 21;
+            label9.Text = "Parts Associated With This Product";
+            // 
+            // addProductBtn
+            // 
+            addProductBtn.Location = new Point(713, 198);
+            addProductBtn.Name = "addProductBtn";
+            addProductBtn.Size = new Size(75, 23);
+            addProductBtn.TabIndex = 22;
+            addProductBtn.Text = "Add";
+            addProductBtn.UseVisualStyleBackColor = true;
+            // 
+            // modifyProductForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 488);
+            Controls.Add(addProductBtn);
+            Controls.Add(label9);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox7);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(saveProductBtn);
+            Controls.Add(cancelProductBtn);
+            Controls.Add(label8);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "modifyProductForm";
+            Text = "modifyProductForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label8;
+        private Button cancelProductBtn;
+        private Button saveProductBtn;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox7;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private Label label9;
+        private Button addProductBtn;
+    }
+}
